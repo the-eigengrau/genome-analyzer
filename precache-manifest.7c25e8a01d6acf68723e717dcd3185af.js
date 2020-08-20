9aaa5f1e7161aa54239cdc72cc9ba50d7bb67f4a
Ayon Bhattacharya <ayon.ux@gmail.com>
2020-08-20 10:23:34 -0500

    Updates

---
+++ precache-manifest.7c25e8a01d6acf68723e717dcd3185af.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8157baa76035e5bb416ff78d64801cf",
+    "url": "/genome-analyzer/index.html"
+  },
+  {
+    "revision": "948d2e658a3701cd36ab",
+    "url": "/genome-analyzer/static/css/2.0642c937.chunk.css"
+  },
+  {
+    "revision": "3fc25d90598f806c2513",
+    "url": "/genome-analyzer/static/css/main.f456db16.chunk.css"
+  },
+  {
+    "revision": "948d2e658a3701cd36ab",
+    "url": "/genome-analyzer/static/js/2.2c62bf51.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/genome-analyzer/static/js/2.2c62bf51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fc25d90598f806c2513",
+    "url": "/genome-analyzer/static/js/main.8d9d61b9.chunk.js"
+  },
+  {
+    "revision": "0901747a0cc8cf97769f",
+    "url": "/genome-analyzer/static/js/runtime-main.6b642371.js"
+  },
+  {
+    "revision": "47047f7c182f3387e2a1cca338aeef0a",
+    "url": "/genome-analyzer/static/media/align.47047f7c.png"
+  },
+  {
+    "revision": "6f16cbf4eab3cb2b45ab597a4a613017",
+    "url": "/genome-analyzer/static/media/background.6f16cbf4.jpg"
+  },
+  {
+    "revision": "be09f54c3a4f5bbf903168aa43d94be1",
+    "url": "/genome-analyzer/static/media/leia.be09f54c.jpg"
+  },
+  {
+    "revision": "9d3de910e28d480b006e4849e8dec771",
+    "url": "/genome-analyzer/static/media/luke.9d3de910.jpg"
+  },
+  {
+    "revision": "0a049e99798368d9e3363784e769122c",
+    "url": "/genome-analyzer/static/media/mace.0a049e99.jpg"
+  },
+  {
+    "revision": "801419d539e51e2ebbd7555af7d3688b",
+    "url": "/genome-analyzer/static/media/tax.801419d5.jpg"
+  }
+]);
